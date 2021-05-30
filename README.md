17e6e4060adbf710205c66dead90a92bf17dc154
jbordalo <j.bordalo@campus.fct.unl.pt>
2021-05-30 20:53:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Watch2Bot
+
+## About the Project
+
+Discord Bot to create [Watch2Gether](https://w2g.tv/) rooms.
+
+## Installing
+
+`pip3 install discord`
+
+`pip3 install python-dotenv`
+
+### Configuration
+
+You must have a `.env` file on the root folder of the bot.
+
+```
+DISCORD_API_KEY=your_discord_api_key
+WATCH_API_KEY=your_watch_2_gether_api_key
+```
+
+## Usage
+
+Run the bot with `python3 watch2bot.py`.
+
+Invoke the bot with `-w <youtube-link>` on your Discord channel.
+
+## Authors
+
+-   **João Bordalo** - _Initial work_ - [jbordalo](https://github.com/jbordalo)
---
+++ watch2bot.py
@@ -0,0 +1,48 @@
+import discord
+import os
+import requests
+from dotenv import load_dotenv
+
+ROOM_URL = 'https://w2g.tv/rooms/'
+PROMPT = '-w '
+YOUTUBE = 'https://www.youtube.com/watch?'
+
+load_dotenv()
+
+url = 'https://w2g.tv/rooms/create.json'
+
+client = discord.Client()
+
+
+def print_error():
+	return "Aw, let's try that again!\nUsage: `-w https://www.youtube.com/watch?v=1zTbVRPh5EI`"
+
+
+def print_room(key):
+	room = "%s%s" % (ROOM_URL, key)
+	return "Here\'s your room, enjoy the party:\n%s" % room
+
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	if message.content.startswith(PROMPT):
+		yt_link = message.content.split()[1]	
+
+		if not yt_link.startswith(YOUTUBE):
+			await message.channel.send(print_error())
+			return
+
+		payload = {'w2g_api_key': os.getenv('WATCH_API_KEY'), 'share': yt_link}
+		r = requests.post(url, data=payload)
+		
+		await message.channel.send(print_room(r.json()['streamkey']))
+
+client.run(os.getenv('DISCORD_API_KEY'))
